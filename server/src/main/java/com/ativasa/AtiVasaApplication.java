b702605d7fe801c97e999706f7dc121e4b66fbba
Anmol-dev21 <anmolbhargav.dev@gmail.com>
2026-01-09 12:02:12 +0530

    Initial project setup: client (Vite) + server (Spring Boot)

---
+++ server/src/main/java/com/ativasa/AtiVasaApplication.java
@@ -0,0 +1,13 @@
+package com.ativasa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtiVasaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtiVasaApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/ativasa/AtiVasaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ativasa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtiVasaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
